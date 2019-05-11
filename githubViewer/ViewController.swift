c3191a2a53dba55d3cd5b9ecfba4ba605d58f19c
Yuto Nakano <yuto.44.bb@gmail.com>
2019-05-11 19:17:17 +0900

    first commit

---
+++ githubViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  githubViewer
+//
+//  Created by 中野湧仁 on 2019/05/11.
+//  Copyright © 2019 Yuto Nakano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
